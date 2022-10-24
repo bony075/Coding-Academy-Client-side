eb21ac36a74df81e629af09e8eeb5d85beed5139
Bony <bony.075@gmail.com>
2022-10-24 22:43:48 +0600

    Route setup

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,38 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../layout/Main";
+import Category from "../../Pages/Category/Category";
+import Course from "../../Pages/Course/Course";
+import Home from "../../Pages/Home/Home";
+import Login from "../../Pages/Login/Login/Login";
+import Register from "../../Pages/Login/Register/Register";
+
+export const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/category/:id',
+                element: <Category></Category>,
+                loader: ({ params }) => fetch(`http://localhost:5000/category_course/${params.id}`)
+            },
+            {
+                path: '/course/:id',
+                element: <Course></Course>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            },
+            {},
+        ]
+    }
+])
