e9b2c7d0a26e290a693e16dc02f2f95bfa90f6cf
Bony <bony.075@gmail.com>
2022-10-25 00:58:54 +0600

    add header with bug

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 import Footer from '../Pages/Shared/Footer/Footer';
 import Header from '../Pages/Shared/Header/Header';
+import LeftsideNav from '../Pages/Shared/LeftsideNav/LeftsideNav';
 
 const Main = () => {
     return (
-        <div>
+        <div className=''>
             <Header></Header>
-            <div>
-            
-            </div>           
+            <div className='grid grid-rows-1 grid-flow-col gap-4 grid-cols-2 container mx-auto'>
+                <div >
+                    <LeftsideNav></LeftsideNav>
+                </div>
+                <div>
+                    <Outlet></Outlet>
+                </div>
+            </div>
             <Footer></Footer>
         </div>
     );
