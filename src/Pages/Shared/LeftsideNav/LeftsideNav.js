c17265592f8f3e4a7bd5cbaaf2e8546b9fda339b
bony075 <bony.075@gmail.com>
2022-10-25 16:07:00 +0600

    fix layout with couser category

--- src/Pages/Shared/LeftsideNav/LeftsideNav.js
+++ src/Pages/Shared/LeftsideNav/LeftsideNav.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftsideNav = () => {
+    const [categoris, setCategoris] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/course-categories')
+            .then(res=>res.json())
+        .then(data=>setCategoris(data))
+    },[]);
     return (
         <div>
-            <h2>this is left side nav</h2>
+            <h2>this is left side nav {categoris.length}</h2>
+            <div>
+                 
+            {
+                    categoris.map(ct => <p key={ct.id}>
+                    
+                        <Link className='link link-primary' to={`/category/${ct.id}`}>{ ct.name}</Link>
+                </p>
+                )
+            }
+           </div>
         </div>
     );
 };
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -8,11 +8,11 @@ const Main = () => {
     return (
         <div className=''>
             <Header></Header>
-            <div className='grid grid-rows-1 grid-flow-col gap-4 grid-cols-2 container mx-auto'>
-                <div >
+            <div className='grid grid-rows-1 gap-4 grid-cols-4 container mx-auto '>
+                <div className=' columns-3xs'>
                     <LeftsideNav></LeftsideNav>
                 </div>
-                <div>
+                <div className=' col-span-3'>
                     <Outlet></Outlet>
                 </div>
             </div>
