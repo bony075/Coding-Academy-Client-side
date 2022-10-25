b82fd1ea937e22c5f74442ee21e231c197cb32c6
Bony <bony.075@gmail.com>
2022-10-26 02:33:02 +0600

    add login register form

--- src/Contexts/AuthProvider/AuthProvider.js
+++ src/Contexts/AuthProvider/AuthProvider.js
@@ -1,9 +1,25 @@
 import React, { createContext } from 'react';
+import { getAuth, signInWithPopup} from 'firebase/auth'
+import app from '../../firebase/firebase.config';
 
 export const AuthContext = createContext();
+const auth= getAuth(app);
+
+
 const AuthProvider = ({ children }) => {
     const user = { displayName: 'test user' };
-    const authinfo={user};
+    const providerLogin = (provider) => {
+        return signInWithPopup(auth,provider);
+    }
+
+
+
+
+
+
+
+
+    const authinfo = { user, providerLogin };
     return (
         <AuthContext.Provider value={authinfo}>
             { children}
--- src/Pages/Shared/LeftsideNav/LeftsideNav.js
+++ src/Pages/Shared/LeftsideNav/LeftsideNav.js
@@ -1,36 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { FaGoogle,FaGithub } from "react-icons/fa";
+import { FaGoogle, FaGithub } from "react-icons/fa";
 
 const LeftsideNav = () => {
     const [categoris, setCategoris] = useState([]);
     useEffect(() => {
         fetch('http://localhost:5000/course-categories')
-            .then(res=>res.json())
-        .then(data=>setCategoris(data))
-    },[]);
+            .then(res => res.json())
+            .then(data => setCategoris(data))
+    }, []);
     return (
         <div>
 
             <div className="btn-group btn-group-vertical">
-  <button className="btn btn-active"><FaGoogle></FaGoogle> Google Sign in</button>
+                <button className="btn btn-active"><FaGoogle></FaGoogle> Google Sign in</button>
 
-  <button className="btn"><FaGithub></FaGithub> GitHub Sign in</button>
-</div>
+                <button className="btn"><FaGithub></FaGithub> GitHub Sign in</button>
+            </div>
 
 
 
             <h2>this is left side nav {categoris.length}</h2>
             <div>
-                 
-            {
+
+                {
                     categoris.map(ct => <p key={ct.id}>
-                    
-                        <Link className='link link-primary' to={`/category/${ct.id}`}>{ ct.name}</Link>
-                </p>
-                )
-            }
-           </div>
+
+                        <Link className='link link-primary' to={`/category/${ct.id}`}>{ct.name}</Link>
+                    </p>
+                    )
+                }
+            </div>
         </div>
     );
 };
